f3867ceb29e201412ca44f06f4d09f1aaf9b7817
Learning Python <154525342+sphadlamz@users.noreply.github.com>
2026-01-22 11:24:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
+import streamlit as st
 
+st.set_page_config(
+    page_title="AscendLink",
+    layout="mobile"
+)
+
+st.title("AscendLink")
+st.subheader("Youth & Women Enterprise Empowerment Platform")
+
+st.write("""
+Connecting entrepreneurs, mentors, and customers
+using Huawei Cloud.
+""")
+
+role = st.selectbox(
+    "Who are you?",
+    ["Entrepreneur", "Mentor", "Customer"]
+)
+
+st.success(f"Welcome, {role}!")
